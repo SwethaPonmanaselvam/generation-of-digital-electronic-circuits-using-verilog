7df5e4e0046c85f7f97093bd7f7a02cbf029060c
SwethaPonmanaselvam <swethaselvam003@gmail.com>
2025-01-18 12:46:16 +0530

    Update jk_ff_tb.v

--- jk_ff_tb.v
+++ jk_ff_tb.v
@@ -1,1 +1,22 @@
+module jk_ff_tb;
+  reg J, K;
+  wire q, qn;
+  reg clk;
+  JK_flipflop uut (.J(J), .K(K), .clk(clk), .q(q), .qn(qn));
+  
+  initial begin
+    clk = 0; 
+    forever #5 clk = ~clk;  
+  end
+
+ initial begin
+    $dumpfile("dump.vcd"); 
+    $dumpvars;  
+    #10 J = 0; K = 0; 
+    #10 J = 0; K = 1; 
+    #10 J = 1; K = 0; 
+    #10 J = 1; K = 1; 
+    #50 $finish;  
+  end
+endmodule
 
