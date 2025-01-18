7f206d0a2d74a92ed968e27bd07f243e2a713e80
SwethaPonmanaselvam <swethaselvam003@gmail.com>
2025-01-18 16:55:27 +0530

    Update pipo_using_dff_tb.v

--- pipo_using_dff_tb.v
+++ pipo_using_dff_tb.v
@@ -1,1 +1,27 @@
 
+module tb_pipo_4bit;
+  reg [3:0] pin;
+  reg clk, load;
+  wire [3:0] pout;
+
+  pipo_4bit uut(.pin(pin), .clk(clk), .load(load), .pout(pout));
+
+  initial begin
+    $dumpfile("pipo_4bit.vcd");
+    $dumpvars(0, tb_pipo_4bit);
+
+    clk = 0;
+    load = 0;
+    pin = 4'b0000;
+
+    #5 load = 1; pin = 4'b1011;
+    #10 load = 0;
+
+    #20 load = 1; pin = 4'b0110;
+    #10 load = 0;
+
+    #50 $finish;
+  end
+
+  always #5 clk = ~clk;
+endmodule
